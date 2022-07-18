62f8bee63f148c97bf2e0b1e01904ee8fd41869c
gamejunkiez <queenangrylobster@gmail.com>
2022-07-18 18:14:42 -0500

    implemented factory pattern

---
+++ FactoryPatternChallenge/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPatternChallenge
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many tires would you like on your vehicle?");
+            var numOfTires = int.Parse(Console.ReadLine());
+            var newVehicle = VehicleFactory.GetVehicle(numOfTires);
+            newVehicle.Drive();
+            
+        }
+    }
+}
---
+++ FactoryPatternChallenge/VehicleFactory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPatternChallenge
+{
+    internal static class VehicleFactory
+    {
+        public static IVehicle GetVehicle(int numOfTires)
+        {
+            IVehicle answer = numOfTires == 2 ? (IVehicle) new Motorcycle() : (IVehicle) new Car();
+            return answer;
+
+        }
+    }
+}
